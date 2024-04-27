c97a268908c0350aea16f2cbb7d940197013cdc8
Greedysky <greedysky@163.com>
2024-04-27 15:48:29 +0800

    refactor: Optimize common singleton module[503451]

--- TTKModule/TTKText/TTKTimestampTransform/mainwindow.cpp
+++ TTKModule/TTKText/TTKTimestampTransform/mainwindow.cpp
@@ -11,11 +11,11 @@ QString MainWindow::dateTimeStringFromTimestampString(const QString &stamp)
 {
     if(stamp.length() == 13)
     {
-        return QDateTime::fromMSecsSinceEpoch(stamp.toLongLong()).toString("yyyy-MM-dd hh:mm:ss");
+        return QDateTime::fromMSecsSinceEpoch(stamp.toLongLong()).toString(TTK_DATE_TIMEZ_FORMAT);
     }
     else if(stamp.length() == 10)
     {
-        return QDateTime::fromMSecsSinceEpoch(stamp.toInt() * 1000).toString("yyyy-MM-dd hh:mm:ss");
+        return QDateTime::fromMSecsSinceEpoch(stamp.toInt() * 1000).toString(TTK_DATE_TIMEZ_FORMAT);
     }
     return {};
 }
